d99e586dc448cf23941e58d07a2fc1aef4233020
josly hamash <145527706+josly-hamash@users.noreply.github.com>
2023-11-23 10:12:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+
+function myFunction(){
+ document.getElementById("clicked-para").innerHTML="cell no:9345435892"+"<br>"+ "whatsapp no:9962893382";
+}
+
+function li1Function(){
+     document.getElementById("li1").innerHTML="WEB DEVELOPER SUMMARY"+"<br>"+"<br>"+" specializing in HTML5, JavaScript, PHP, MySQL, database design,"+
+    '<br>' +"and front-end development. Adept at collaborating with dynamic teams to build "
+    +"<br>"+ "high-quality websites and identify opportunities to enhance the user experience."
+}
+
+function li2Function(){
+    document.getElementById("li1").innerHTML=
+    "DETAILS"+"<BR>"+"<br>"+
+    "Current Location : chennai"+"<br>"+
+    "Email Id         : josleewiz10101997@gmail.com"+"<br>"+
+    "Mobile no        : 9345435892"+"<br>"+
+    "Whatsapp no      : 9962893382"+"<br>"+
+    "DOB              : 10 OCT 1997"+"<br>"+
+    "Language  : Tamil, English"
+
+}
+
+function li4Function(){
+    document.getElementById("li1").innerHTML="cell no:9345435892"+"<br>"+ "whatsapp no:9962893382";
+}
